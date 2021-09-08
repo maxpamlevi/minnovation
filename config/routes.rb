7446148c35b44565dd80d9b49cfdc2b3653d0313
jason nguyen <jasonnguyen@motorist.sg>
2021-09-08 21:21:14 +0700

    update UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'product/:id/:title' => 'home#product'
   get 'shopping' => 'home#shopping'
+  get 'auction' => 'home#auction'
+  get 'store' => 'home#store'
+  get 'growth' => 'home#growth'
+
+
+
 end

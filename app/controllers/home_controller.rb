9a65e8d30abf32d6b2bb61859f6c4ce140113149
jason nguyen <jasonnguyen@motorist.sg>
2021-09-20 16:41:54 +0700

    update some view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ class HomeController < ApplicationController
         @new_products = Product.where('thumbnail like "%drive%"').where('discount = 0').limit(2)
         @sale_products = Product.where('thumbnail like "%drive%"').where('discount > 0').order(:created_at).limit(2)
         @just_for_you_products = Product.where('thumbnail like "%drive%"').order('RAND()').limit(2)
-        @best_buy_product = Product.where('thumbnail like "%drive%"').order('RAND()').first
+        @best_buy_product = Product.where('thumbnail like "%drive%"').order('RAND()').limit(3)
     end
 
     def brand

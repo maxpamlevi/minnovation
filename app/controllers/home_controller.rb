89652dd57055c3b9b9fe5d6949f35be9ccf69ca5
jason nguyen <jasonnguyen@motorist.sg>
2021-09-28 16:04:55 +0700

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,9 @@ class HomeController < ApplicationController
         @sale_products = Product.where('thumbnail like "%drive%"').where('discount > 0').order(:created_at).limit(4).each_slice(2).to_a
         @just_for_you_products = Product.where('thumbnail like "%drive%"').order('RAND()').limit(2)
         @best_buy_product = Product.where('thumbnail like "%drive%"').order('RAND()').limit(3)
+
+        @make_data = Product.all.pluck(:vehicle_make).uniq
+        @color_data = Product.all.pluck(:color).uniq
     end
 
     def brand
@@ -19,11 +22,15 @@ class HomeController < ApplicationController
     end
 
     def search
-
+        @products = Product.all
+        @products = @products.where("name like '%#{params[:keyword]}%'") if params[:keyword].present?
     end
 
     def auction
         @auctions = Auction.includes(:product).where.not(status: :closed)
+
+        @make_data = Product.pluck(:vehicle_make).uniq
+        @color_data = Product.pluck(:color).uniq
     end
 
     def store

5aaf6831a8f725242b3645f291053292c1dc541d
jason nguyen <jasonnguyen@motorist.sg>
2021-11-15 13:22:40 +0700

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
 
     def shopping
         @new_products = Product.where('thumbnail like "%drive%"').where(state: 0).limit(4).each_slice(2).to_a
-        @sale_products = Product.where('thumbnail like "%drive%"').where('discount > 0').order(:created_at).limit(4).each_slice(2).to_a
+        @sale_products = Product.where('thumbnail like "%drive%"').order(:created_at).limit(4).each_slice(2).to_a
         @just_for_you_products = Product.where('thumbnail like "%drive%"').limit(4)
         @best_buy_product = Product.where('thumbnail like "%drive%"').limit(3)
 
